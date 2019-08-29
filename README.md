6f0cca05f09a0c7f29e073bbbd768f09cab1756a
Danny <daniel@danw.website>
2019-08-29 17:33:42 +0300

    split

--- README.md
+++
@@ -1,1 +0,0 @@
-# danyisill.github.io
--- ez/fabs.sh
+++
@@ -1,6 +0,0 @@
-#structure: ./games/en/game x.yz
-#the last word MUST BE version number
-#output: ./en-game-x.yz.zip + .dmg -> inside is just game.app
-easyrpg-player -v 2> /dev/null
-mkdir -p temp
-find games/* -maxdepth 0 -type d -exec ./mkicns.sh {} \;
